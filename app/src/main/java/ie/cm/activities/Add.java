e456db72e29c3cecbb30045bdcf5b8fb38e5512e
CRea7 <conorrea123@gmail.com>
2019-01-23 15:39:08 +0000

    Add working lab1 complete

--- app/src/main/java/ie/cm/activities/Add.java
+++ app/src/main/java/ie/cm/activities/Add.java
@@ -1,9 +1,15 @@
 package ie.cm.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 import ie.cm.R;
+import ie.cm.models.Coffee;
 
 public class Add extends Base {
 
@@ -11,5 +17,31 @@ public class Add extends Base {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add);
+    }
+
+    public void addCoffee(View view)
+    {
+        EditText name = findViewById(R.id.addNameET);
+        EditText shop = findViewById(R.id.addShopET);
+        EditText price = findViewById(R.id.addPriceET);
+        RatingBar ratingBar = findViewById(R.id.addRatingBar);
+
+        double coffeePrice;
+
+
+        String coffeeName = name.getText().toString();
+        String coffeeShop = shop.getText().toString();
+        try {
+            coffeePrice = Double.parseDouble(price.getText().toString());
+        }
+        catch(NumberFormatException e){
+            coffeePrice = 0.0;
+        }
+        float ratingValue = ratingBar.getRating();
+
+        Coffee c = new Coffee(coffeeName, coffeeShop, ratingValue, coffeePrice, false);
+        coffeeList.add(c);
+        //Log.d("Coffee", coffeeList.get(0).toString());
+        //startActivity(new Intent(this,Home.class));
     }
 }
--- app/src/main/java/ie/cm/activities/Home.java
+++ app/src/main/java/ie/cm/activities/Home.java
@@ -1,61 +1,64 @@
 package ie.cm.activities;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import ie.cm.R;
+import ie.cm.activities.Add;
+import ie.cm.activities.Base;
+import ie.cm.models.Coffee;
 
 public class Home extends Base {
+
+    TextView emptyList;
+    ListView coffeeListView;
+    ArrayAdapter<Coffee> coffeeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        emptyList = findViewById(R.id.emptyList);
+        coffeeListView = findViewById(R.id.recentlyAddedList);
+        coffeeListView.setEmptyView(emptyList);
+        coffeeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,coffeeList);
+        coffeeListView.setAdapter(coffeeAdapter);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Information", Snackbar.LENGTH_LONG)
+                        .setAction("More Info...", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                            }
+                        }).show();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.menu_help) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void add(View v)
-    {
+    public void add(View v) {
         startActivity(new Intent(this, Add.class));
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("coffeemate","Home : " + coffeeList);
+
+        if(!coffeeList.isEmpty())
+            coffeeAdapter.notifyDataSetChanged();
+    }
 }
